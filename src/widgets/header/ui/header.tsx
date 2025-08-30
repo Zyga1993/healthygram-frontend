2724d02abe746286226cba3bd3fe545a4d21e3e9
Omar <omareabdo@gmail.com>
2025-08-31 00:04:02 +0200

    A refresher on CSS grid

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,3 +1,19 @@
+import { Navbar } from "./navbar";
+import "./header.css";
+
 export const Header = () => {
-  return <div>Das ist unsera Header.</div>;
+  return (
+    <>
+      <div className="grid">
+        <div className="row">
+          <div className="col">
+            <p>Das ist unsera header</p>
+          </div>
+          <div className="col">
+            <Navbar />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
