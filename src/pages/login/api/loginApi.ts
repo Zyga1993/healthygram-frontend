f94dc985be4ce9e06dfa3cce80a090d58d765c79
Omar <omareabdo@gmail.com>
2025-09-02 21:08:47 +0200

    replacing axios with firebase

--- src/pages/login/api/loginApi.ts
+++ src/pages/login/api/loginApi.ts
@@ -1,7 +1,17 @@
-import axios from "axios"
+// import axios from "axios"
 import type { LoginFormType } from "../model/types"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
 export const loginApi = async (loginForm: LoginFormType) => {
-    const request = await axios.post('http://localhost:3000/login', loginForm)
-    console.log(request)
+    const auth = getAuth()
+    const { email, password } = loginForm
+    const request = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+    )
+    console.log(request.operationType, request.providerId, request.user)
+    return request.user
+
 }
+
