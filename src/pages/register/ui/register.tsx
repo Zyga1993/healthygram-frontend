5661e3307739288f8698ab6115ac027d18778b6c
Omar <omareabdo@gmail.com>
2025-09-02 23:03:00 +0200

    Refactoring the local state in the register component from a user object to indivdual elements states

--- src/pages/register/ui/register.tsx
+++ src/pages/register/ui/register.tsx
@@ -1,23 +1,20 @@
 import { useState } from "react";
-import type { RegisterFormType } from "../model/types";
 import { useRegister } from "../model/registerMutation";
 
 export const RegisterForm = () => {};
 
 export const RegisterPage = () => {
-  const [user, setUser] = useState<RegisterFormType>({
-    email: "",
-    password: "",
-  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const registerMutation = useRegister();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const response = await registerMutation.mutateAsync(user);
+      const response = await registerMutation.mutateAsync({ email, password });
       console.log(response);
-      console.log("Success", user.email, user.password);
+      console.log("Success", { email, password });
     } catch (error) {
       console.log(error);
     }
@@ -30,16 +27,19 @@ export const RegisterPage = () => {
         <input
           type="email"
           placeholder="Email"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Register</button>
+
+        <button type="submit" disabled={registerMutation.isPending}>
+          {registerMutation.isPending ? "Loading..." : "Register"}
+        </button>
       </form>
     </>
   );
